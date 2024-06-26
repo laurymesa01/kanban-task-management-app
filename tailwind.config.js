718c25fedc372be6084b0d510d9d81a9b4e92373
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-06-26 11:08:21 -0500

    Setting main colors for the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'main-purple': '#635FC7',
+      'main-purple-hover': '#A8A4FF',
+      'black': '#000112',
+      'very-dark-grey': '#20212C',
+      'dark-grey': '#2B2C37',
+      'lines-dark': '#3E3F4E',
+      'medium-grey': '#828FA3',
+      'lines-light': '#E4EBFA',
+      'light-grey': '#F4F7FD',
+      'white': '#FFFFFF',
+      'red': '#EA5555',
+      'red-hover': '#FF9898'
+    }
   },
   plugins: [],
 }
