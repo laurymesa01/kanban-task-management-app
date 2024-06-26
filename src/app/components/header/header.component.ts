b0dfb2b4c3eedfe96a18ec0507f65abd9b1543d5
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-06-26 17:25:20 -0500

    Working on header component with responsive

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { distinctUntilChanged, tap } from 'rxjs';
+
 
 @Component({
   selector: 'app-header',
@@ -8,5 +11,32 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  buttonAdd: string = '';
+
+  Breakpoints = Breakpoints;
+  readonly breakpoint$ = this.breakpointObserver
+    .observe(['(min-width: 1024px)', '(min-width: 768px)', '(max-width: 768px)'])
+    .pipe(
+      tap(value => console.log(value)),
+      distinctUntilChanged()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver){}
+
+  ngOnInit() {
+    this.breakpoint$.subscribe(() =>
+    this.breakpointChanged()
+    );
+  }
+
+  private breakpointChanged(){
+    if (this.breakpointObserver.isMatched('(min-width: 768px)')) {
+      this.buttonAdd = '+ Add New Task';
+    }
+    else if(this.breakpointObserver.isMatched('(max-width:768px)')){
+      this.buttonAdd = '';
+    }
+  }
 
 }
