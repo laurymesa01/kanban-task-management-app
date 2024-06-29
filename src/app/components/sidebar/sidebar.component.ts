4dd637d5a9ebdeeca159a665368513a77e742009
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-06-28 22:19:24 -0500

    Sidebar component showing all boards

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { BoardService } from '../../services/board.service';
+import { Board } from '../../models/board.model';
+import { UpperCasePipe } from "@angular/common";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+
+  boards = signal<Board[]>([]);
+  boards_service = inject(BoardService);
+
+  ngOnInit(){
+    this.boards_service.getBoards().subscribe({
+      next: (boards) => {
+        this.boards.set(boards)
+      }
+    })
+  }
 
 }
