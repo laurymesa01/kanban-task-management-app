f4b84955d2602ee22e096c376f32c11dcccb1a58
Laura Elena Mesa <mesafernandezlauraelena@gmail.com>
2024-08-02 16:48:23 -0500

    Add new columns in creating new board form

--- src/app/components/new-board/new-board.component.ts
+++ src/app/components/new-board/new-board.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule, FormArray, FormArrayName } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+import { Board } from '../../models/board.model';
 @Component({
   selector: 'app-new-board',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './new-board.component.html',
   styleUrl: './new-board.component.scss'
 })
-export class NewBoardComponent {
+export class NewBoardComponent implements OnInit{
+
+
+  boardFormGroup: FormGroup = this._formBuilder.group({
+    name: new FormControl("", [Validators.required, Validators.minLength(1)]),
+    columns: this._formBuilder.array([])
+  })
+
+  constructor(private _formBuilder: FormBuilder){
+    const column = this._formBuilder.group({
+      name: new FormControl("Todo"),
+      tasks: new FormControl([]),
+    });
+    const column2 = this._formBuilder.group({
+      name: new FormControl("Doing"),
+      tasks: new FormControl([]),
+    });
+    this.columns().push(column);
+    this.columns().push(column2);
+  }
+
+  ngOnInit(){
+  }
+
+  columns():FormArray{
+    return this.boardFormGroup.get('columns') as FormArray;
+  }
+
+  addColumn(){
+    const column = this._formBuilder.group({
+      name: new FormControl(""),
+      tasks: new FormControl([]),
+    })
+    this.columns().push(column)
+  }
+
+
+
+  deleteColumn(){
+
+  }
 
 }
