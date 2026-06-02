6cecb2176520ce7e19a84c5f2189a01372349bb0
laurymesa01 <mesafernandezlauraelena@gmail.com>
2026-06-02 12:49:02 -0700

    feat: add Sidebar component with board list and controls

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,25 @@
+const Sidebar = () => {
+  return (
+    <aside>
+        <p>All boards( 3 )</p>
+        <div>
+            <div>
+                <span>Platform Launch</span>
+            </div>
+            <div>
+                <span>Marketing Plan</span>
+            </div>
+            <div>
+                <span>Roadmap</span>
+            </div>
+        </div>
+        <button>Create new board</button>
+        <div>
+            <button></button>
+        </div>
+        <button>Hide Sidebar</button>
+    </aside>
+  )
+}
+
+export default Sidebar
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,9 +1,11 @@
 import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
 
 const Layout = () => {
   return (
     <main>
         <Header/>
+        <Sidebar/>
     </main>
   )
 }
